b23e1a2a83c81b89adb5c8dda312e15fa62c71d6
AARYAN JINDAL <aaryanjindal15@gmail.com>
2026-02-10 12:50:58 +0530

    Fix Vercel serverless deployment

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ app.post("/bfhl", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+export default app;
